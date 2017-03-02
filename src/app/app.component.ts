61dd7c53fef3a95a82dfce1b2c2dabab88f1a040
SAPIENT\vku145 <vikas10145.12@bitmesra.ac.in>
2017-03-02 18:54:13 +0530

    Added tab and http get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { ApiService } from '../services';
 
 import '../style/app.scss';
-
+import '../style/custom-blue.css';
 @Component({
   selector: 'my-app', // <my-app></my-app>
   templateUrl: './app.component.html',
+  // styleUrls:['homepage.css'],
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
--- src/app/dialog/dialog-child-content.ts
+++ src/app/dialog/dialog-child-content.ts
@@ -1,4 +1,6 @@
 import {Component,Injectable} from '@angular/core';
+import {Http ,  Response} from '@angular/http';
+
 
 @Injectable()
 @Component({
@@ -6,4 +8,13 @@ import {Component,Injectable} from '@angular/core';
   templateUrl: './dialog-child-content.html',
   styleUrls: ['./dialog-child-content.css']
 })
-export class DialogChildContent {}
+export class DialogChildContent {
+
+    results: Object;
+    constructor(http: Http){
+      this.results = [];
+      http.get('https://jsonplaceholder.typicode.com/users').map((res: Response) => res.json())
+      .subscribe(res => this.results = res);
+    }
+
+}
